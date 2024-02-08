bd38a350b8742f96d40f78a6c28835d3fa9ea957
leonardoolia <leonardooliaa@gmail.com>
2024-02-08 14:02:11 +0100

    created pokemon component

--- src/AppMain.vue
+++ src/AppMain.vue
@@ -1,9 +1,15 @@
 <script>
+import PokemonList from './components/pokemons/PokemonList.vue';
 export default {
     name: 'AppMain',
+    components: { PokemonList }
 }
 </script>
 
-<main></main>
+<template>
+    <main class="container">
+        <PokemonList />
+    </main>
+</template>
 
 <style></style>
---
+++ src/components/pokemons/PokemonList.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'PokemonList'
+}
+</script>
+
+<template>
+    <section id="pokemon"></section>
+</template>
+
+<style></style>
